102aa561ae16bec7c481acb7ec9ad1dc975ea6fe
Jairo Duarte <alfredojairo17@hotmail.com>
2018-02-19 00:04:05 +0000

    language.js function changeLangue, conflanguage, getlanguage add

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,25 @@
 const fs = require('fs');
-var output = require('./language.json');
-//var content = JSON.stringify(output);
-console.log(output);
-console.log(output.Language);
-//console.log(content);
-/*
-fs.readFile('file', 'utf8', function (err, data) {
-  if (err) throw err;
-  obj = JSON.parse(data);
-});
+var content = require('./language.json');
 
-async function changeLangue() {
-    
-}*/
+function changelanguage (language = 'Eng') {
+  content.Language = language;
+  const output = JSON.stringify(output);
+  fs.writeFile("./language.json", output, 'utf8',(err) => { if (err) return console.log(err);});
+}
+
+function conflanguage(language) {
+  content.config = true;
+  changelangue(language);
+}
+
+function getlanguage() {
+  if (content.Language == 'Pt') {
+    return content.Pt;
+  }
+  else if (content.Language == 'Eng') {
+    return content.Eng;
+  }
+  else
+    return content.Fr;
+}
+module.exports.language = {chageLangue, conflanguage, getlanguage}
