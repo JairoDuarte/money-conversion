adfc7206d72aa6e9d62f665dcb4ec88741374439
Jairo Duarte <alfredojairo17@hotmail.com>
2018-02-20 02:05:09 +0000

    function menu add

--- src/app.js
+++ src/app.js
@@ -1,69 +1,80 @@
 #!/usr/bin/env node
-const { scanf } = require('nodejs-scanf');
+const scanf = require('scanf');
 const {app} = require('./index');
 const {language} = require('./utils')
 const colors = require('colors/safe');
-
+const _this = this;
 const Console = console;
+var choice = true,cx;
+
+function print (params, params1) {
+  Console.log(colors.blue(' %s') + colors.magenta(' is  %s'), params, params1);
+}
 function init() {
   if (!language.getconfig()) {
     language.init(colors);
-    scanf('%d',(choice)=>{
-      console.log(choice);
-      switch (choice) {
-        case 1:
-          language.conflanguage('Eng');
-          break;
-        case 2:
-          language.conflanguage('Fr');
-          break;
-        case 3:
-          language.conflanguage('Pt');
-          break;
-        default:
-          language.changelanguage('Eng');
-      }
-    }); 
+    var choice = scanf('%d');
+    switch (choice) {
+      case 1:
+        language.conflanguage('Eng');
+        break;
+      case 2:
+        language.conflanguage('Fr');
+        break;
+      case 3:
+        language.conflanguage('Pt');
+        break;
+      default:
+        language.changelanguage('Eng');
+    }
   }
 }
 init();
 const language_ = language.getlanguage();
 
-var choice = false;
-while (choice) {
-  console.log(language_.form1);
-  console.log(`1 - ${language_.choice1}`);
-  console.log(`2 - ${language_.choice2}`);
-  console.log(`3 - ${language_.choice3}`);
-  scanf('%d',(cx) =>{
+async function menu() {
+    do{
+    console.log(colors.green(language_.form1));
+    console.log(colors.bold(`1 - ${language_.choice1}`));
+    console.log(colors.bold(`2 - ${language_.choice2}`));
+    console.log(colors.bold(`3 - ${language_.choice3}`));
+    cx = scanf('%d');
     switch (cx) {
-      case 1:
-        
+      case 1:{
+        var print = function (params, params1) {
+          Console.log(colors.blue(' %s') + colors.magenta(' is  %s'), params, params1);
+        }
+        var t = scanf('%s %s %s');
+        var from = t[0];
+        var to = t[1];
+        var value = t[2];
+        console.log(typeof(_this));
+        await app.result(from.toUpperCase(),to.toUpperCase(),value,print);
         break;
+      }
       case 2:
+        await app.help();
         break;
       case 3:
-        break;
-      default:
-        
+         return false;
+      default: 
     }
-  })
+  /*
+    Console.log(colors.green('Please input value in format: from to money'));
+    Console.log(colors.italic.red('Ex: USD EUR 100'));
+    Console.log(colors.bold(':help  -> if you need help'));
+    scanf('%s %s %d', (from, to, value) => {
+      if (from != '' && to != '' && value != '' )
+        app.result(from.toUpperCase(), to.toUpperCase(), value, print);
+        console.log(to);
+    }); */
 
-  Console.log(colors.green('Please input value in format: from to money'));
-  Console.log(colors.italic.red('Ex: USD EUR 100'));
-  Console.log(colors.bold(':help  -> if you need help'));
+
+  }while (choice);
 }
-
+menu();
 
 // input currency change and value maney
-const print = function print(params, params1) {
-  Console.log(colors.blue(' %s') + colors.magenta(' is  %s'), params, params1);
-};
-init();
-scanf('%s %s %d', (from, to, value) => {
-  if (from.toUpperCase() === ':HELP') {
-    app.help();
-  } else {
-    app.result(from.toUpperCase(), to.toUpperCase(), value, print);
-  }
-});
+
+
+
