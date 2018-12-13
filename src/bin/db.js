f8c4424aea94dd222e4130e181ddb173ea54c01b
Jairo Duarte <alfredojairo17@hotmail.com>
2018-12-13 16:29:09 +0100

    db.js api acces key stutus add

--- src/bin/db.js
+++ src/bin/db.js
@@ -4,7 +4,8 @@ const fs =  require ('fs');
 var content = require('./db.json');
 
 class Database {
-  
+
+	// Save language chosen by user
 	static savelanguage (language) {
 		content.language = language.name;
 		content.status = language.status;
@@ -13,11 +14,32 @@ class Database {
 			if (err) return console.log(err); 
 		}); 
 	}
+
+	// Save language chosen by user
+	static savecont (cont) {
+		content.cont_api_key = cont;
+		const output = JSON.stringify(content);
+		fs.writeFile(__dirname +'/db.json', output, 'utf8', (err) => {
+			if (err) return console.log(err); 
+		}); 
+	}
   
+	// return the default language 
 	static content () {
 		return content;
 	}
 
+	// return true if user number conversion without access key <=10 or false
+	static api_key(){
+		let value = content.cont_api_key;
+		if (value >10 ) {
+			return false;
+		} else {
+			value++;
+			Database.savecont(value);
+			return true;
+		}
+	}
 }
 
 module.exports = Database;
